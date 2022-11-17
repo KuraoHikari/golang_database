41ca127f98db58563bb03e00a155b8147b3dad87
KuraoHikari <dewaindra705@gmail.com>
2022-11-17 13:16:26 +0800

    first commit

---
+++ database.go
@@ -0,0 +1,21 @@
+package belajar_golang_database
+
+import (
+	"database/sql"
+	"time"
+)
+
+func GetConnection() *sql.DB {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/belajar_golang_database?parseTime=true")
+
+	if err != nil {
+		panic(err)
+	}
+
+	
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+	return db
+}
